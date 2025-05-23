a3725fe6ed6a013aee41570e56e807aaaad528fd
sankars1997 <111554844+sankars1997@users.noreply.github.com>
2025-05-23 16:16:42 +0530

    Create app.blade.php

---
+++ app.blade.php
@@ -0,0 +1,29 @@
+<header id="header" class="header d-flex align-items-center fixed-top">
+    <div class="container-fluid container-xl position-relative d-flex align-items-center">
+
+      <a href="index.html" class="logo d-flex align-items-center me-auto">
+        <!-- Uncomment the line below if you also wish to use an image logo -->
+        <!-- <img src="assets/img/logo.webp" alt=""> -->
+        <h1 class="sitename"></h1>
+      </a>
+
+      <nav id="navmenu" class="navmenu">
+        <ul>
+          
+          <li><a href="#about">About</a></li>
+          <li><a href="#services">Services</a></li>
+          
+          
+          </li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+        <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
+      </nav>
+
+      <a class="btn-getstarted" href="login">Login</a>
+      <a class="btn-getstarted" href="signup">Signup</a>
+
+    </div>
+  </header>
+
+    

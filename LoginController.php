f69216170ac0fcfbcf11f7f6698486daf06294aa
sankars1997 <111554844+sankars1997@users.noreply.github.com>
2025-05-23 16:08:15 +0530

    Update LoginController.php

--- LoginController.php
+++ LoginController.php
@@ -1,10 +1,16 @@
+renamed as admin controller
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\User;
+use App\Models\District;
+use App\Models\State;
+use App\Models\College;
+
 
 use Illuminate\Http\Request;
 
-class logincontroller extends Controller
+class Admincontroller extends Controller
 {
     public function adminlogin(){
         return view ('login');
@@ -28,7 +34,13 @@ class logincontroller extends Controller
         auth()->logout();
         return view ('login');
     }
+     
 
+    public function adduser(){
+        return view('adduser');
+    }
+
+    
 
     public function search(){
         if(auth()->check()){
@@ -40,14 +52,48 @@ class logincontroller extends Controller
     }
 
 
-    public function adduser(){
-        
-        return view ('adduser');
-    }
+    
 
 
     public function addsupplier(){
         
         return view ('addsupplier');
     }
+
+    public function addlaptop(){
+        
+        return view ('addlaptop');
+    }
+    public function addcollege(){
+        $districts = District::all(); 
+        $states = State::all(); 
+    return view('addcollege', compact('districts', 'states'));
+      }
+      
+      public function registercollege(Request $request){
+
+        $validated = $request->validate([
+            'collegeid'     => 'required|string|max:50|unique:colleges,collegeid',
+            'collegename'   => 'required|string|max:100',
+            'address'       => 'required|string',
+            'state'         => 'required|string|exists:states,id',
+            'district'      => 'required|string|exists:districts,id',
+            'email'         => 'required|email|unique:colleges,email',
+            'mobilenumber'  => 'required|digits:10',
+        ]);
+
+
+
+        College::create([
+            'collegeid' => $request->collegeid,
+            'collegename' => $request->collegename,
+            'address' => $request->address,
+            'state' => $request->state,
+            'district' => $request->district,
+            'email' => $request->email,
+            'mobilenumber' => $request->mobilenumber,
+            
+        ]);
+        return redirect()->route('addcollege')->with('message','college added successfully');;
+      }
 }
